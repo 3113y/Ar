98c9ed93573b2b3af0d69005ecfb0c994980ccba
3113y <448592981@qq.com>
2025-03-25 19:09:03 +0800

    Add files via upload

---
+++ Arknights/EID.lua
@@ -0,0 +1,6 @@
+---@diagnostic disable: undefined-global
+EID:addCollectible(Crown,"↑{{Damage}} 当血量不高于1时攻击力+,如果玩家集齐了三件及以上的国王收藏品，则攻击力+++#（偶尔存在BUG使该效果失效）","诸王的冠冕",cn)
+EID:addCollectible(New_Lance,"↑{{Tears}} 当血量不高于1时射速++#（目前有BUG导致面板不显示改变的数值）","国王的新枪",cn)
+EID:addCollectible(Armor,"每进入一个房间后使红心-、魂心+，该效果不会使血量降低至0","国王的铠甲",cn)
+EID:addCollectible(Legacy,"飞舞道具，没用","国王的延伸",cn)
+EID:addCollectible(Hatred,"↓{{Damage}}攻击力-（不会写敌人攻击力上升的效果，只能出此下策）","死仇时代的恨意",cn)
--- Arknights/headfile.lua
+++ Arknights/headfile.lua
@@ -1,6 +1,14 @@
+---@diagnostic disable: undefined-global, lowercase-global
+mod = RegisterMod("Ar", 1)
+player = Game():GetPlayer(0)
 New_Lance = Isaac.GetItemIdByName("King's New Lance")
 Legacy = Isaac.GetItemIdByName("King's Legacy")
 Armor = Isaac.GetItemIdByName("King's Armor")
 Crown = Isaac.GetItemIdByName("King's Crown")
-i = 1
-j = 1
+New_Lance_Count = player:GetCollectibleNum(New_Lance)
+Legacy_Count = player:GetCollectibleNum(Legacy)
+Armor_Count = player:GetCollectibleNum(Armor)
+Crown_Count = player:GetCollectibleNum(Crown)
+King_Count = New_Lance_Count + Legacy_Count + Armor_Count + Crown_Count
+King_i = 1
+King_j = 1
